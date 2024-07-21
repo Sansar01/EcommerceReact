187a7507a155b7936432b8db6aa950e8779b8921
Sansar <you@example.com>
2024-07-21 21:45:26 +0530

    "Update Hero and Offers component styles, add Offers and NewCollection components to Shop page"

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../component/Hero/Hero'
 import Popular from '../component/Popular/Popular'
+import Offers from '../component/Offers/Offers'
+import NewCollection from '../component/NewCollection/NewCollection'
 
 function Shop() {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers/>
+      <NewCollection/>
     </div>
   )
 }
